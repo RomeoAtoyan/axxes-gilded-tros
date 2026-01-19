3783b5ba3d1d11947fe5a6bd42b86b772c148a0e
RomeoAtoyan <atoyanr@gmail.com>
2026-01-19 11:03:18 +0100

    refactor: extracted item variable inside loop for readability

--- src/gilded-tros.ts
+++ src/gilded-tros.ts
@@ -1,63 +1,67 @@
-import {Item} from './item';
+import { Item } from "./item";
 
 export class GildedTros {
+  constructor(public items: Array<Item>) {}
 
-    constructor(public items: Array<Item>) {
+  public updateQuality(): void {
+    for (let i = 0; i < this.items.length; i++) {
+      const item = this.items[i];
 
-    }
-
-    public updateQuality(): void {
-        for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Good Wine' && this.items[i].name != 'Backstage passes for Re:Factor'
-                && this.items[i].name != 'Backstage passes for HAXX') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'B-DAWG Keychain') {
-                        this.items[i].quality = this.items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1;
-
-                    if (this.items[i].name == 'Backstage passes for Re:Factor') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1;
-                            }
-                        }
-
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (this.items[i].name != 'B-DAWG Keychain') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
-
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Good Wine') {
-                    if (this.items[i].name != 'Backstage passes for Re:Factor' || this.items[i].name != 'Backstage passes for HAXX') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'B-DAWG Keychain') {
-                                this.items[i].quality = this.items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality;
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1;
-                    }
-                }
-            }
+      if (
+        item.name != "Good Wine" &&
+        item.name != "Backstage passes for Re:Factor" &&
+        item.name != "Backstage passes for HAXX"
+      ) {
+        if (item.quality > 0) {
+          if (item.name != "B-DAWG Keychain") {
+            item.quality = item.quality - 1;
+          }
         }
+      } else {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+
+          if (item.name == "Backstage passes for Re:Factor") {
+            if (item.sellIn < 11) {
+              if (item.quality < 50) {
+                item.quality = item.quality + 1;
+              }
+            }
+
+            if (item.sellIn < 6) {
+              if (item.quality < 50) {
+                item.quality = item.quality + 1;
+              }
+            }
+          }
+        }
+      }
+
+      if (item.name != "B-DAWG Keychain") {
+        item.sellIn = item.sellIn - 1;
+      }
+
+      if (item.sellIn < 0) {
+        if (item.name != "Good Wine") {
+          if (
+            item.name != "Backstage passes for Re:Factor" ||
+            item.name != "Backstage passes for HAXX"
+          ) {
+            if (item.quality > 0) {
+              if (item.name != "B-DAWG Keychain") {
+                item.quality = item.quality - 1;
+              }
+            }
+          } else {
+            item.quality =
+              item.quality - item.quality;
+          }
+        } else {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
+          }
+        }
+      }
     }
-
+  }
 }
-
